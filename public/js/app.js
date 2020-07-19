d7a4e3737d9b0a6eb1cd66236c3e1619ea63b061
Rashi <rashii8791@gmail.com>
2020-07-20 00:46:09 +0530

    Removing unnecessary console log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
-
-console.log('Javascript from public directory');
-
-// fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
