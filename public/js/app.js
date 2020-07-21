96052410f331553054c03be58c75456bbb8f2ffd
Rashi <rashii8791@gmail.com>
2020-07-21 16:42:11 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ async function getForecast(location)
 {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
-    const response = await fetch(`http://localhost:3000/weather?address=${location}`);
+    const response = await fetch(`/weather?address=${location}`);
     const data = await response.json();
     if(data.error)
     messageOne.textContent = (`Error: ${data.error}`);
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geoCode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -90,6 +91,6 @@ app.get('*', (req, res) => {
 });
 
 // Set up the port
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up and listening.');
 });
